20f269c57340350d07a63aa4cf052f20e0d67674
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2026-06-25 20:17:35 -0500

    Remove confirmation prompt for page reload on new version availability

--- versionCheck.js
+++ versionCheck.js
@@ -2,7 +2,5 @@
 const worker = new Worker('/versionCheck.worker.js');
 
 worker.onmessage = ({ data }) => {
-  if (confirm(`A new version is available (${data.version}).\r\n\r\nThe most recent change was '${data.message}'\r\n\r\nIs it OK to reload the page?`)) {
-    location.reload(true);
-  }
+  location.reload(true);
 };
